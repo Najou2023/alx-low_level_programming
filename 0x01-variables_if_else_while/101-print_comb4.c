8405a915488e7fd1da42142df995d74e7d3ddb83
Najlaa <najlaaidrissi@gmail.com>
2023-03-17 11:13:07 -0700

    The success combination in business

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,37 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
- * main - main function
- *
- * Return: always 0
+ * main - Entry point
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	int i;
-	int j;
-	int k;
+	int d, p, q;
 
-	for (i = 0 ; i < 10 ; i++)
+	for (d = '0' ; d < '9' ; d++)
 	{
-		for (j = 1 ; i < 10 ; j++)
-		{
-			for (k = 2 ; i < 10 ; k++)
-			{
-				if (i < j && j < k)
-				{
-					putchar(i + '0');
-					putchar(j + '0');
-					putchar(k + '0');
-					if (i + j + k != 24)
-					{
-						putchar(',');
-						putchar(' ');
 
-					}
-				}
-			}
-		}
+	for (p = d + 1; p <= '9' ; p++)
+	{
+
+	for (q = p + 1; q <= '9' ; q++)
+	{
+	if ((p != d) != q)
+	{
+	putchar(d);
+	putchar(p);
+	putchar(q);
+	if (d == '7' && p == '8')
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
 	}
 	putchar('\n');
 	return (0);
